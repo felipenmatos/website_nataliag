756e47de49cf1294b7ae1592c68c81cae1c790a1
felipenmatos <notfelipe2@hotmail.com>
2022-12-15 19:27:41 -0300

    ✖️ fix: styling test with deploy

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className="container">
       <img
         className={styles.imgProfile}
         src="/assets/Natalia.jpg"
